632709157f36b98173b34625ba275f47932d306f
AnsibleGuy <guy@ansibleguy.net>
2023-01-04 19:45:04 +0100

    basic structure

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ Role to provision NFTables firewall on linux servers.
 [![Molecule Test Status](https://badges.ansibleguy.net/infra_nftables.molecule.svg)](https://molecule.readthedocs.io/en/latest/)
 [![YamlLint Test Status](https://badges.ansibleguy.net/infra_nftables.yamllint.svg)](https://yamllint.readthedocs.io/en/stable/)
 [![Ansible-Lint Test Status](https://badges.ansibleguy.net/infra_nftables.ansiblelint.svg)](https://ansible-lint.readthedocs.io/en/latest/)
-[![Ansible Galaxy](https://img.shields.io/ansible/infra_nftables/GALAXY_ID)](https://galaxy.ansible.com/ansibleguy/infra_nftables)
-[![Ansible Galaxy Downloads](https://img.shields.io/badge/dynamic/json?color=blueviolet&label=Galaxy%20Downloads&query=%24.download_count&url=https%3A%2F%2Fgalaxy.ansible.com%2Fapi%2Fv1%2Froles%2FGALAXY_ID%2F%3Fformat%3Djson)](https://galaxy.ansible.com/ansibleguy/infra_nftables)
+[![Ansible Galaxy](https://img.shields.io/ansible/role/61265)](https://galaxy.ansible.com/ansibleguy/infra_nftables)
+[![Ansible Galaxy Downloads](https://img.shields.io/badge/dynamic/json?color=blueviolet&label=Galaxy%20Downloads&query=%24.download_count&url=https%3A%2F%2Fgalaxy.ansible.com%2Fapi%2Fv1%2Froles%2F61265%2F%3Fformat%3Djson)](https://galaxy.ansible.com/ansibleguy/infra_nftables)
 
 
 **Tested:**
@@ -45,6 +45,9 @@ NFTables: [Wiki](https://wiki.nftables.org/wiki-nftables/index.php/Quick_referen
 
 ## Info
 
+* **Info:** Read the [Hook documentation](https://wiki.nftables.org/wiki-nftables/index.php/Netfilter_hooks) to know when and how to configure hooks and priorities!
+
+
 * **Note:** this role currently only supports debian-based systems
 
 
@@ -64,7 +67,6 @@ For this role to work - you must install its dependencies first:
 ansible-galaxy install -r requirements.yml
 ```
 
-
 ## Usage
 
 ### Config
@@ -73,12 +75,34 @@ Define the config as needed:
 
 ```yaml
 nftables:
+  _defaults:
+    table:
+      type: 'inet'
+    
+    chain:
+      policy: 'drop'
+      type: 'filter'
+      priority: 0
+      log:
+        drop: true
 
-```
-
-You might want to use 'ansible-vault' to encrypt your passwords:
-```bash
-ansible-vault encrypt_string
+  tables:
+    internet:
+      chains:
+        incoming:
+          hook: 'input'
+    
+        outgoing:
+          hook: 'output'
+          policy: 'accept'
+    
+        route:
+          hook: 'forward'
+    
+        translate:
+          hook: 'postrouting'
+          type: 'nat'
+          policy: 'accept'
 ```
 
 ### Execution
@@ -89,8 +113,7 @@ ansible-playbook -K -D -i inventory/hosts.yml playbook.yml
 ```
 
 There are also some useful **tags** available:
-* 
-*
+* config
 
 To debug errors - you can set the 'debug' variable at runtime:
 ```bash
--- filter_plugins/utils.py
+++ filter_plugins/utils.py
@@ -1,20 +1,159 @@
+from re import sub as regex_replace
+
+
 class FilterModule(object):
 
     def filters(self):
         return {
             "nftables_rules_translate": self.nftables_rules_translate,
             "nftables_rules_sort": self.nftables_rules_sort,
+            "nftables_format_list": self.nftables_format_list,
+            "nftables_format_var": self.nftables_format_var,
+            "nftables_safe_name": self.nftables_safe_name,
+            "ensure_list": self.ensure_list,
+            "extend_list": self.extend_list,
         }
 
     @staticmethod
-    def nftables_rules_translate(raw_rules: list, cofnig: dict) -> list:
+    def ensure_list(data: (str, list)) -> list:
+        if isinstance(data, list):
+            return data
+
+        else:
+            return [data]
+
+    @classmethod
+    def extend_list(cls, l1: list, l2: list) -> list:
+        l1 = cls.ensure_list(l1)
+        l1.extend(cls.ensure_list(l2))
+        return l1
+
+    @staticmethod
+    def nftables_safe_name(name: str) -> str:
+        return regex_replace('[^0-9a-zA-Z]+', '', name.replace(' ', '_'))
+
+    @classmethod
+    def nftables_rules_translate(cls, raw_rules: list, config: dict) -> list:
         rules = []
+        NONE_VALUES = ['', ' ', None]
+
+        for r in raw_rules:
+            if isinstance(r, str):
+                _rule = r
+
+            elif not isinstance(r, dict):
+                raise ValueError(
+                    'Rule has unsupported format! Should be string or dict!'
+                    f"Rule: {r}'"
+                )
+
+            elif 'raw' in r:
+                _rule = r['raw']
+
+            else:
+                _translation = config['defaults'].copy()
+                _field_mapping = {}
+
+                for field_nft, fields_config in config['aliases'].items():
+                    for field_config in fields_config:
+                        if field_config in r:
+                            _field_mapping[field_nft] = field_config
+                            _value = r[field_config]
+
+                            if isinstance(_value, list):
+                                _value = cls.nftables_format_list(_value)
+
+                            elif field_nft in config['quote'] and _value.find('"') == -1:
+                                _value = f'"{_value}"'
+
+                            if isinstance(_value, str):
+                                _value.strip()
+
+                            if field_nft not in NONE_VALUES:
+                                _translation[field_nft] = f"{field_nft} {_value}"
+
+                            else:
+                                _translation[field_nft] = _value
+
+                if config['drop_log'] and _translation['action'] == 'drop' and 'log prefix' not in _field_mapping:
+                    # add generic logging for any dropped packets
+                    if 'comment' in _translation:
+                        _translation['log prefix'] = f"log prefix \"{_translation['comment']}\""
+
+                    else:
+                        _translation['log prefix'] = 'log'
+
+                _rule = ''
+                for field_nft in config['sequence']:
+                    if field_nft in _translation:
+                        _rule += f"{_translation[field_nft]} "
+
+                _rule = _rule.strip()
+
+            if _rule in NONE_VALUES:
+                continue
+
+            rules.append(_rule)
 
         return rules
 
-
     @staticmethod
     def nftables_rules_sort(raw_rules: list, config: dict) -> list:
-       rules = []
+        rules = []
+        ordered = {}
 
-       return rules
+        for r in raw_rules:
+            _seq = None
+
+            if isinstance(r, dict):
+                for k in config['fields']:
+                    if k in r:
+                        _seq = r[k]
+                        break
+
+            if _seq is None:
+                _seq = config['fallback']
+                config['fallback'] += 1
+
+            if _seq in ordered:
+                raise ValueError(f"Got duplicate rule sequence: '{_seq}' in rule: {r}")
+
+            ordered[_seq] = r
+
+        sequence = list(ordered.keys())
+        sequence.sort()
+
+        for i in sequence:
+            rules.append(ordered[i])
+
+        return rules
+
+    @classmethod
+    def nftables_format_list(cls, data: list) -> str:
+        return f"{{ {', '.join(map(str, cls.ensure_list(data)))} }}"
+
+    @classmethod
+    def nftables_format_var(cls, key: str, value: (str, list)) -> str:
+        return f"define { cls.nftables_safe_name(key) } = {cls.nftables_format_list(value)}"
+
+    @classmethod
+    def nftables_format_set(cls, config: dict, whitespace: int) -> str:
+        lines = []
+
+        if 'typeof' in config:
+            lines.append(f"typeof {config['typeof']}")
+
+        else:
+            lines.append(f"type {config['type']}")
+
+        _flags = cls.ensure_list(config['flags'])
+        if len(_flags) > 0:
+            lines.append(f"flags {', '.join(map(str, cls.ensure_list(_flags)))}")
+
+            for k, v in config['settings'].items():
+                lines.append(f"{k} {v}")
+
+        if config['counter']:
+            lines.append('counter')
+
+        return f";\n{' ' * whitespace}".join(lines)
