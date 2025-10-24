2582f762bc89de3bcd7620973ee97d44e792d419
Rath Pascal <rath@oxl.at>
2025-10-25 01:23:46 +0200

    disable counter for sets by default

--- filter_plugins/utils.py
+++ filter_plugins/utils.py
@@ -14,6 +14,10 @@ class FilterModule(object):
 
     @staticmethod
     def ensure_list(data: (str, list)) -> list:
+        # if user supplied a string instead of a list => convert it to match our expectations
+        if data is None:
+            return []
+
         if isinstance(data, list):
             return data
 
